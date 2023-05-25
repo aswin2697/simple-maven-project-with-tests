642cd566503dddf56f7b5dda105042911a656d57
ashwin <aswinkumar2697@gmail.com>
2023-05-25 11:46:39 +0530

    change the access modifier from protect to public

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -29,7 +29,7 @@ import org.junit.internal.AssumptionViolatedException;
 
 class Base {
 
-    protected void run() {
+    public void run() {
         double r = Math.random();
         if (r < 0.1) {
             fail("oops");
